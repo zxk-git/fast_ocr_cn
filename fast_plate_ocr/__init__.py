c9f2421d18ab6a51e3b45bb237aa96f9e3000147
zxk <zxk.zdg666888@gmail.com>
2026-07-28 11:11:18 +0800

    init

--- fast_plate_ocr/__init__.py
+++ fast_plate_ocr/__init__.py
@@ -1,20 +1,14 @@
+'''
+Author: zxk zxk.zdg666888@gmail.com
+Date: 2026-07-28 11:11:31
+LastEditors: zxk zxk.zdg666888@gmail.com
+LastEditTime: 2026-07-28 11:11:53
+'''
 """
 fast-plate-ocr package.
 """
 
-from typing import TYPE_CHECKING
-
 from fast_plate_ocr.core.types import PlatePrediction
-
-if TYPE_CHECKING:
-    from fast_plate_ocr.inference.plate_recognizer import LicensePlateRecognizer
+from fast_plate_ocr.inference.plate_recognizer import LicensePlateRecognizer
 
 __all__ = ["LicensePlateRecognizer", "PlatePrediction"]
-
-
-def __getattr__(name: str) -> object:
-    if name == "LicensePlateRecognizer":
-        from fast_plate_ocr.inference.plate_recognizer import LicensePlateRecognizer
-
-        return LicensePlateRecognizer
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
